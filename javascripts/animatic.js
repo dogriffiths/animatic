975e1d91fa1f737a84ba26aa295e8d75fe5c30f9
dogriffiths <dogriffiths@ae1e6cf0-2c2b-5229-0c9c-22473bd8dbe0>
2010-10-03 10:02:55 +0000

    Fixed some messed up regexps
    git-svn-id: https://animatic.googlecode.com/svn/trunk@23 ae1e6cf0-2c2b-5229-0c9c-22473bd8dbe0

--- javascripts/animatic.js
+++ javascripts/animatic.js
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2010 David Griffiths, http://code.google.com/p/animatic/
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -8,10 +8,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -28,181 +28,198 @@ var _animatic_Attributes = [];
 // These are the major functions.
 //
 
-function animate(obj, attrName, targetValue, howManySecs, animatorToUse) {
-  var animator = animatorToUse || runner;
-  var t = howManySecs || 0.25;
-  animatorFn = animator(t, obj[attrName], targetValue);
-  _animatic_animateWithAnimator(obj, attrName, animatorFn);
+function animate(obj, attrName, targetValue, howManySecs, animatorToUse)
+{
+    var animator = animatorToUse || runner;
+    var t = howManySecs || 0.25;
+    animatorFn = animator(t, obj[attrName], targetValue);
+    _animatic_animateWithAnimator(obj, attrName, animatorFn);
 }
 
-function drift(obj, heading, speedValue, wrapValue, maxXValue, maxYValue) {
-  var speed = speedValue || 100;
-  var maxX = maxXValue || window.innerWidth;
-  var maxY = maxYValue || window.innerHeight;
-  var wrap = wrapValue;
-  if (wrap == null) {
-    wrap = true;
-  }
-  var attrNameY = "y";
-  if (obj.top) {
-    attrNameY = "top";
-  }
-  animatorFn = _animatic_drifterY(speed, obj[attrNameY], heading, wrap, maxY);
-  _animatic_animateWithAnimator(obj, attrNameY, animatorFn);
-  var attrNameX = "x";
-  if (obj.left) {
-    attrNameX = "left";
-  }
-  animatorFn = _animatic_drifterX(speed, obj[attrNameX], heading, wrap, maxX);
-  _animatic_animateWithAnimator(obj, attrNameX, animatorFn);
-}
-
-function stopAnimation(obj, attrNameValue) {
-  var attrName = attrNameValue || "";
-  for (var i = _animatic_Objects.length - 1; i >= 0; i--) {
-    var o = _animatic_Objects[i];
-    var a = _animatic_Attributes[i];
-    if ((o == obj) && ((a == attrName) || (attrName = ""))) {
-      _animatic_Objects.splice(i, 1);
-      _animatic_Attributes.splice(i, 1);
+function drift(obj, heading, speedValue, wrapValue, maxXValue, maxYValue)
+{
+document.title = "here I am";
+    var speed = speedValue || 100;
+    var maxX = maxXValue || window.innerWidth;
+    var maxY = maxYValue || window.innerHeight;
+    var wrap = wrapValue;
+    if (wrap == null) {
+        wrap = true;
     }
-  }
+    var attrNameY = "y";
+    if (obj.top) {
+        attrNameY = "top";
+    }
+    animatorFn = _animatic_drifterY(speed, obj[attrNameY], heading, wrap, maxY);
+    _animatic_animateWithAnimator(obj, attrNameY, animatorFn);
+    var attrNameX = "x";
+    if (obj.left) {
+        attrNameX = "left";
+    }
+    animatorFn = _animatic_drifterX(speed, obj[attrNameX], heading, wrap, maxX);
+    _animatic_animateWithAnimator(obj, attrNameX, animatorFn);
+}
+
+function stopAnimation(obj, attrNameValue)
+{
+    var attrName = attrNameValue || "";
+    for (var i = _animatic_Objects.length - 1; i >= 0; i--) {
+        var o = _animatic_Objects[i];
+        var a = _animatic_Attributes[i];
+        if ((o == obj) && ((a == attrName) || (attrName = ""))) {
+            _animatic_Objects.splice(i, 1);
+            _animatic_Attributes.splice(i, 1);
+        }
+    }
 }
 
 //
 // And these are the rest
 //
 
-function _animatic_animateWithAnimator(obj, attrName, animatorFn) {
-  obj["animatic_" + attrName] = animatorFn;
-  for (var i = _animatic_Objects.length - 1; i >= 0; i--) {
-    var o = _animatic_Objects[i];
-    var a = _animatic_Attributes[i];
-    if ((o == obj) && (a == attrName)) {
-      _animatic_Objects.splice(i, 1);
-      _animatic_Attributes.splice(i, 1);
+function _animatic_animateWithAnimator(obj, attrName, animatorFn)
+{
+    obj["animatic_" + attrName] = animatorFn;
+    for (var i = _animatic_Objects.length - 1; i >= 0; i--) {
+        var o = _animatic_Objects[i];
+        var a = _animatic_Attributes[i];
+        if ((o == obj) && (a == attrName)) {
+            _animatic_Objects.splice(i, 1);
+            _animatic_Attributes.splice(i, 1);
+        }
     }
-  }
-  _animatic_Objects.push(obj);
-  _animatic_Attributes.push(attrName);
+    _animatic_Objects.push(obj);
+    _animatic_Attributes.push(attrName);
 }
 
 // Now repaint every 10 ms.
-setInterval(function() { _animatic_updateAll() }, 10);
+setInterval(function()
+{
+    _animatic_updateAll()
+}, 10);
 
-function _animatic_updateAll() {
-  for (i in _animatic_Objects) {
-    var obj = _animatic_Objects[i];
-    var attrName = _animatic_Attributes[i];
-    var units = unitsFor(obj[attrName]);
-    var newValue = obj["animatic_" + attrName]()
-    obj[attrName] = addUnitsTo("" + newValue, units);
-  }
-}
-
-function _animatic_stripUnits(s) {
-    return ("" + s).replace(/[a-z%]/ig, "");
-}
- 
-function unitsFor(s) {
-    return ("" + s).replace(/[0-9.-]+/ig, "?");
-}
- 
-function addUnitsTo(s, u) {
-    if (u == "?") {
-      return eval(s);
+function _animatic_updateAll()
+{
+    for (i in _animatic_Objects) {
+        var obj = _animatic_Objects[i];
+        var attrName = _animatic_Attributes[i];
+        var units = unitsFor(obj[attrName]);
+        var newValue = obj["animatic_" + attrName]()
+                       obj[attrName] = addUnitsTo("" + newValue, units);
     }
-    return u.replace(/\?/ig, "" + s);
-}
- 
-function runner(p, fromValue, toValue) {
-  var v1 = eval(_animatic_stripUnits(fromValue + ""));
-  var v2 = eval(_animatic_stripUnits(toValue + ""));
-  var now = (new Date()).valueOf();
-  var then = now + (p * 1000);
-  function counterClosure() {
-	var justNow = (new Date()).valueOf();
-	if (justNow >= then) {
-	  return v2;
-	}
-	var prop = (justNow - now) / (then - now);
-	var currently = v1 + ((1-Math.sqrt(1-prop)) * (v2 - v1));
-	return currently
-  }
-  return counterClosure;
 }
 
-function _animatic_drifterX(speedValue, startXValue, headingValue, wrap, maxXValue) {
-  var speed = speedValue;
-  var startX = eval(_animatic_stripUnits(startXValue + ""));
-  var heading = eval(_animatic_stripUnits(headingValue + ""));
-  var maxX = eval(_animatic_stripUnits(maxXValue + ""));
-  var now = (new Date()).valueOf();
-  function counterClosure() {
-	var justNow = (new Date()).valueOf();
+function _animatic_stripUnits(s)
+{
+    return ("" + s).replace( /[a-z%]/ig, "");
+}
+
+function unitsFor(s)
+{
+    return ("" + s).replace( /[0-9.-]+/ig, "?");
+}
+
+function addUnitsTo(s, u)
+{
+    if (u == "?") {
+        return eval(s);
+    }
+    return u.replace( /\?/ig, "" + s);
+}
+
+function runner(p, fromValue, toValue)
+{
+    var v1 = eval(_animatic_stripUnits(fromValue + ""));
+    var v2 = eval(_animatic_stripUnits(toValue + ""));
+    var now = (new Date()).valueOf();
+    var then = now + (p * 1000);
+    function counterClosure() {
+        var justNow = (new Date()).valueOf();
+        if (justNow >= then) {
+            return v2;
+        }
+        var prop = (justNow - now) / (then - now);
+        var currently = v1 + ((1 - Math.sqrt(1 - prop)) * (v2 - v1));
+        return currently;
+    }
+    return counterClosure;
+}
+
+function _animatic_drifterX(speedValue, startXValue, headingValue, wrap, maxXValue)
+{
+    var speed = speedValue;
+    var startX = eval(_animatic_stripUnits(startXValue + ""));
+    var heading = eval(_animatic_stripUnits(headingValue + ""));
+    var maxX = eval(_animatic_stripUnits(maxXValue + ""));
+    var now = (new Date()).valueOf();
+    function counterClosure() {
+        var justNow = (new Date()).valueOf();
         var diff = (justNow - now);
         var cx = startX + diff * speed * Math.sin(heading * Math.PI / 180.0) / 1000;
         if (wrap) {
             while (cx < 0) {
-		cx = cx + maxX;
+                cx = cx + maxX;
             }
             if (cx > maxX) {
-		cx = cx % maxX;
-//		now = now + ((maxXValue - startX) / speed);
+                cx = cx % maxX;
+//      now = now + ((maxXValue - startX) / speed);
             }
         }
-	return cx
-  }
-  return counterClosure;
+        return cx;
+    }
+    return counterClosure;
 }
 
 //
 // The animator factories
 //
 
-function _animatic_drifterY(speedValue, startYValue, headingValue, wrap, maxYValue) {
-  var speed = speedValue;
-  var startY = eval(_animatic_stripUnits(startYValue + ""));
-  var heading = eval(_animatic_stripUnits(headingValue + ""));
-  var maxY = eval(_animatic_stripUnits(maxYValue + ""));
-  var now = (new Date()).valueOf();
-  function counterClosure() {
-	var justNow = (new Date()).valueOf();
+function _animatic_drifterY(speedValue, startYValue, headingValue, wrap, maxYValue)
+{
+    var speed = speedValue;
+    var startY = eval(_animatic_stripUnits(startYValue + ""));
+    var heading = eval(_animatic_stripUnits(headingValue + ""));
+    var maxY = eval(_animatic_stripUnits(maxYValue + ""));
+    var now = (new Date()).valueOf();
+    function counterClosure() {
+        var justNow = (new Date()).valueOf();
         var diff = (justNow - now);
         var cy = startY - diff * speed * Math.cos(heading * Math.PI / 180.0) / 1000;
         if (wrap) {
             while (cy < 0) {
-		cy = cy + maxY;
+                cy = cy + maxY;
             }
             if (cy > maxY) {
-		cy = cy % maxY;
-		now = now + ((maxYValue - startY) / speed);
+                cy = cy % maxY;
+                now = now + ((maxYValue - startY) / speed);
             }
         }
-	return cy
-  }
-  return counterClosure;
+        return cy;
+    }
+    return counterClosure;
 }
 
-function _animatic_rotator(p) {
-  var now = (new Date()).valueOf();
-  var then = now + (p * 1000);
-  function counterClosure() {
-	var justNow = (new Date()).valueOf();
-	if (justNow >= then) {
-	  now = justNow;
-	  then = now + (p * 1000);
-	}
-	var prop = (justNow - now) / (then - now);
-	return prop * 360.0;
-  }
-  return counterClosure;
+function _animatic_rotator(p)
+{
+    var now = (new Date()).valueOf();
+    var then = now + (p * 1000);
+    function counterClosure() {
+        var justNow = (new Date()).valueOf();
+        if (justNow >= then) {
+            now = justNow;
+            then = now + (p * 1000);
+        }
+        var prop = (justNow - now) / (then - now);
+        return prop * 360.0;
+    }
+    return counterClosure;
 }
 
-function _animatic_springer(p, min, scale) {
-  var f = _animatic_rotator(p);
-  function counterClosure() {
-	return scale * (Math.sin(f() * Math.PI / 180.0) + min);
-  }
-  return counterClosure;
+function _animatic_springer(p, min, scale)
+{
+    var f = _animatic_rotator(p);
+    function counterClosure() {
+        return scale * (Math.sin(f() * Math.PI / 180.0) + min);
+    }
+    return counterClosure;
 }
